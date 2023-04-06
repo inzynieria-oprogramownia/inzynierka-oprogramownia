e6d84dc439895cbfa74595e5eb9c97323f8041c3
theWebCookie <jabwojtek@wp.pl>
2023-04-06 11:43:46 +0200

    footer

---
+++ src/components/sections/Footer.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import '../../styles/footer.css'
+
+const Footer = () => (
+  <footer className="footer">
+    <p>&copy; NutriPlan 2023. Wszelkie prawa zastrzeżone</p>
+  </footer>
+)
+
+export default Footer
--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { Provider } from 'react-redux'
 import App from './App'
+import Footer from './components/sections/Footer'
 import './index.css'
 import './styles/theme-variables.css'
 import store from './redux/store'
@@ -10,6 +11,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
       <App />
+      <Footer />
     </Provider>
   </React.StrictMode>
 )
