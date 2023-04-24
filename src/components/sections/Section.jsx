6f5b15148e126f8a9dd3fa48a6283292223f1529
theWebCookie <jabwojtek@wp.pl>
2023-04-24 11:21:28 +0200

    section component

---
+++ src/components/sections/Section.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import '../../styles/section.css'
+import img2 from '../../assets/welcomePage/profile1.jfif'
+import img1 from '../../assets/welcomePage/profile2.jfif'
+
+const Section = () => (
+  <div className="section">
+    <div className="section-box">
+      <div>
+        <p>
+          Dzięki NutriPlan nie musisz rezygnować z ulubionych potraw, a
+          jednocześnie możesz uniknąć niezdrowych nawyków żywieniowych.
+        </p>
+      </div>
+      <img src={img1} alt="profile1" />
+    </div>
+    <div className="section-box">
+      <div>
+        <p>
+          NutriPlan to aplikacja żywieniowa, która zyskała zaufanie tysięcy
+          klientów. Dzięki jej innowacyjnym rozwiązaniom, użytkownicy mogą
+          kontrolować swoją dietę i osiągać zamierzone cele zdrowotne.
+        </p>
+      </div>
+      <img src={img2} alt="profile2" />
+    </div>
+  </div>
+)
+
+export default Section
